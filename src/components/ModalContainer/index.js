6a3057058dc474d3a2ba11e685cad5b690b07d60
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-04-08 15:30:25 -0400

    Add closeModal function

--- src/components/ModalContainer/index.js
+++ src/components/ModalContainer/index.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View, Modal, Text } from 'react-native';
+import { AiOutlineCloseCircle } from "react-icons/ai"
 
-const ModalContainer = ({visible, title, Component}) =>{
+const ModalContainer = ({visible, title, Component, onModalClose}) =>{
 
+    const closeModal = useCallback(()=>{
+        onModalClose(false);
+    }, [onModalClose])
 
     return(
         <Modal visible={visible} animationType="slide">
             <View style={{ flexDirection:"row", alignItems:"center" }}>
-                x
+                <AiOutlineCloseCircle onPress={closeModal} style={{ margin: 10 }} />
                 <Text style={{ fontSize: 22 }}>{title}</Text>
             </View>
             <>
