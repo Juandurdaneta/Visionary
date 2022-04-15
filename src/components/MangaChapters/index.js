3c2717eb9685fcfa4b868c4735ff0d335f4bf7f7
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-04-15 19:04:39 -0400

    Update on mangaChapters component

--- src/components/MangaChapters/index.js
+++ src/components/MangaChapters/index.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { useChapterFetch } from "../../Hooks/useChapterFetch";
 
 const MangaChapters = ({mangaId}) =>{
 
-    const { state: chapters, loading, error} = useChapterFetch(mangaId)
+    const { state: chapters, loading, error} = useChapterFetch(mangaId);
 
-    console.log(chapters)
+    let cover = chapters.length > 0 && chapters[0].chapterImages[0];
+
+   if(cover){
+       console.log(cover)
+   }
+    
 
     return(
         <View>
-            <Text>{mangaId}</Text>
+           { chapters.length > 0 ?
+            <Image style={{width: 100, height: 100}}source={`data:image/${cover.contentType};base64,${cover.data}`}/>
+            : <Text>Loading..</Text>
+        }
         </View>
     )
 
